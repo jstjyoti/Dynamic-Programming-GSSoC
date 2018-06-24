4428926e67ff7f24073c0d4390c27535a0aef9f0
Jyoti Mishra <37954338+jstjyoti@users.noreply.github.com>
2018-06-24 14:02:39 +0530

    Update coin_change.cpp

--- Algorithms/coin_change.cpp
+++ Algorithms/coin_change.cpp
@@ -1,15 +1,11 @@
 int count( int S[], int m, int n )
 {
-
-    int table[n+1];
- 
-    memset(table, 0, sizeof(table));
- 
-    table[0] = 1;
- 
+        int table[n+1];
+     memset(table, 0, sizeof(table));//making all the elements of array table 0
+    table[0] = 1;//making first element as 1
     for(int i=0; i<m; i++)
         for(int j=S[i]; j<=n; j++)
             table[j] += table[j-S[i]];
- 
+    //return the coin change array
     return table[n];
 }
